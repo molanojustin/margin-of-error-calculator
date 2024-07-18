0bbb2e68b1af1262d2156782fb58edc38d386546
molanojustin <160653314+molanojustin@users.noreply.github.com>
2024-07-18 17:22:43 -0400

    Updated moeFun.py to include additional documentation

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 This is a simple script that calculates the overall margin of error assuming a 95% confidence interval and a proportion percentage of 0.5. Both can be altered when calling the function.
 
 This will also export to a csv, but a formatted xlsx with the default settings is included in the repo.
+
+The output includes margin of errors for every sample size 40 and above at a multiple of 5 until 400 after which the multiple is changed to 10 until 2000 is reached.
+
+To run:
+- Open `moeFun.py`
+- Define z-score depending on desired confidence level.
+- Define p depending on proportion percentage.
+- Update the range and multiples to include.
+- Run `moeFun.py`
--- moeFun.py
+++ moeFun.py
@@ -1,18 +1,12 @@
 import math
 import csv
 
+# Calculates MOE based on a z-score of 1.96 (95% CI) and a proportion of 0.5
 def calculateMOE(n, z=1.96, p=0.5):
     moe = z * math.sqrt((p*(1-p)))/math.sqrt(n)
     return moe
 
-sampleSizeList = list(range(38, 2001))
-sampleSizeList = [x for x in sampleSizeList if (x < 400 and x % 5 == 0) or (x >= 400 and x % 10 == 0)]
-
-moeDict = {}
-for sampleSize in sampleSizeList:
-    result = calculateMOE(sampleSize)
-    moeDict[sampleSize] = result
-
+# Defines the function to export to excel as a csv
 def export_dict_to_csv(data, csv_filename):
     with open(csv_filename, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
@@ -20,9 +14,25 @@ def export_dict_to_csv(data, csv_filename):
         for key, value in data.items():
             writer.writerow([key, value])
 
-# Example usage:
+# Calls function
 if __name__ == '__main__':
-    csv_filename = 'marginOfError.csv'
-    export_dict_to_csv(moeDict, csv_filename)
+    
+    # Creates a list of sample sizes between 38 and 2000
+    sampleSizeList = list(range(38, 2001))
+
+    # Filters the list to only include multiples of 5 below 400 and multiples of 10 above 400
+    sampleSizeList = [x for x in sampleSizeList if (x < 400 and x % 5 == 0) or (x >= 400 and x % 10 == 0)]
+
+    # Initializes dictionary
+    moeDict = {}
+
+    # For each n, a sample size is calculated and the key/value are added to the dictionary
+    for sampleSize in sampleSizeList:
+        z = 1.96
+        p = 0.5
+        result = calculateMOE(sampleSize, z, p)
+        moeDict[sampleSize] = result
+        csv_filename = 'marginOfError.csv'
+        export_dict_to_csv(moeDict, csv_filename)
     print(f"Dictionary exported to '{csv_filename}' successfully.")
 
